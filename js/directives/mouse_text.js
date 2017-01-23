eff9b60e06982dc861d58673ab93b8821ef5c55a
Jessica G <gillan.jessica@gmail.com>
2017-01-23 12:01:13 -0700

    start mouse text

--- js/directives/mouse_text.js
+++ js/directives/mouse_text.js
@@ -2,6 +2,15 @@ DM.directive('mouseText', function() {
 
   return {
     templateUrl: 'js/directives/mouse_text.html',
-    restrict: 'E'
+    restrict: 'E',
+    link: function(scope, element, attrs){
+      scope.handleMouseDown = function(){
+        element[0].querySelector('#mouse-status').innerHTML = "Mouse DOWN";
+      }
+
+      scope.handleMouseUp = function(){
+        element[0].querySelector('#mouse-status').innerHTML = "Mouse UP";
+      }
+    }
   }
 });
